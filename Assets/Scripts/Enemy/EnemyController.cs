f846bac5a70c2024306f3014259c064d1112da2b
mudyro <michal22.091@gmail.com>
2024-01-27 13:31:00 +0100

    Set up player and enemy movement

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -8,11 +9,11 @@ public class EnemyController : MonoBehaviour
     GameObject player;
 
     [SerializeField]
-    float maxDisplacement = 0.1f,
-    playerPersonalSpaceRadius = 3f,
+    float maxDisplacement = 0.06f,
+    playerPersonalSpaceRadius = 2f,
+    clostEnoughtToDestinationRadius = 0.5f,
     enemyReactionTime = 0.2f,
-    dashStrenght = 8;
-
+    dashStrenght = 10;
 
     bool isEnemyDashing;    
 
@@ -20,61 +21,69 @@ public class EnemyController : MonoBehaviour
     UnityEngine.Vector2 playerPosition;
     UnityEngine.Vector2 enemyPosition;
     UnityEngine.Vector2 currentBallDestination;
-
     UnityEngine.Vector2 closestGoalPosition;
+    UnityEngine.Vector2 goalPosition;
 
     void OnEnable()
     {
         player = GameObject.Find("Player");
         closestGoalPosition = GameObject.FindObjectOfType<Goal>().closestGoal;
+        playerPersonalSpaceRadius += clostEnoughtToDestinationRadius;
     }
 
     void FixedUpdate()
     {
         closestGoalPosition = GameObject.FindObjectOfType<Goal>().closestGoal;   
+        CalculateObjectsPositions();
         
-        if(!isEnemyDashing)
+        if(!EnemyReachedPlayerPersonalSpace() || !EnemyReachedDashPosition())
         {
-            if(player.GetComponent<Rigidbody2D>().velocity != UnityEngine.Vector2.zero)
-            {
-                StartCoroutine(SetPositionForDash());
-            }
-
-            if(enemyPosition == currentBallDestination)
-            {
-                GetComponent<Rigidbody2D>().velocity = UnityEngine.Vector2.zero;
-                StartCoroutine(Dash());
-            }
+            StartCoroutine(SetPositionForDash());
+        }
+        else if(!isEnemyDashing && EnemyReachedDashPosition())
+        {
+            StartCoroutine(Dash());
         }
     }
 
     IEnumerator Dash()
     {
         isEnemyDashing = true;
-        GetComponent<Rigidbody2D>().AddForce(playerToGoalDirection * dashStrenght);
-        yield return new WaitForSeconds(2);
+        GetComponent<Rigidbody2D>().velocity = UnityEngine.Vector2.zero;
+        yield return new WaitForSeconds(0.3f);
+        GetComponent<Rigidbody2D>().AddForce(playerToGoalDirection * dashStrenght, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1.2f);
+        GetComponent<Rigidbody2D>().velocity = UnityEngine.Vector2.zero;
         isEnemyDashing = false;
-
     }
 
     IEnumerator SetPositionForDash()
     {
         yield return new WaitForSeconds(enemyReactionTime);
+        transform.position = UnityEngine.Vector2.MoveTowards(transform.position,currentBallDestination,maxDisplacement);
+    }
 
+    bool EnemyReachedDashPosition()
+    {
+        return (UnityEngine.Vector2.Distance(enemyPosition,currentBallDestination) < clostEnoughtToDestinationRadius);
+    }
+
+    bool EnemyReachedPlayerPersonalSpace()
+    {
+        return UnityEngine.Vector2.Distance(enemyPosition,playerPosition) < playerPersonalSpaceRadius;
+    }
+
+    void CalculateObjectsPositions()
+    {
         playerPosition = new UnityEngine.Vector2(player.transform.position.x,player.transform.position.y);
         enemyPosition = new UnityEngine.Vector2(transform.position.x,transform.position.y);
 
-        UnityEngine.Vector2 goalPosition = closestGoalPosition;
+        goalPosition = closestGoalPosition;
 
         // Calculate player To goal vector.
         playerToGoalDirection = (goalPosition - playerPosition).normalized * playerPersonalSpaceRadius;
 
         currentBallDestination = playerPosition - playerToGoalDirection;
-
-        if(UnityEngine.Vector2.Distance(playerPosition,enemyPosition) > playerPersonalSpaceRadius)
-        {
-            yield return new WaitForSeconds(0.1f);
-            transform.position = UnityEngine.Vector2.MoveTowards(transform.position,currentBallDestination,maxDisplacement);
-        }
+        Debug.DrawLine(currentBallDestination,currentBallDestination + playerToGoalDirection);
     }
 }
